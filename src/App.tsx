215a3c15254ea4d29803c0a3731245e1de59f612
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-03-20 11:12:54 +0100

    refactor: Refactored Button and Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Button from './components/button/button'
+import Text from './components/text/text'
+
 function App() {
-    return <span>Hello</span>
+    return (
+        <Button>
+            <Text color='light-peach' type='body'>
+                some text
+            </Text>
+        </Button>
+    )
 }
 
 export default App
--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import { Color } from '../types'
+import styles from './text.module.scss'
 
 interface TextProps {
     type: 'h1' | 'h2' | 'h3' | 'body'
@@ -14,18 +15,55 @@ enum FontSize {
     body = '16px'
 }
 
-export default function Text({ type, children }: TextProps) {
-    return (
-        <span
-            style={{
-                fontSize: FontSize[type],
-                fontWeight: type === 'body' ? 400 : 500,
-                lineHeight: type === 'h1' || type === 'h2' ? '48px' : '26px',
-                wordSpacing:
-                    type === 'h2' ? '+2px' : type === 'h3' ? '+5px' : '+0px'
-            }}
-        >
-            {children}
-        </span>
-    )
+export default function Text({ color, type, children }: TextProps) {
+    switch (type) {
+        case 'h1':
+            return (
+                <h1
+                    style={{
+                        color: `var(--${color})`,
+                        fontSize: FontSize[type]
+                    }}
+                    className={styles['text__h1']}
+                >
+                    {children}
+                </h1>
+            )
+        case 'h2':
+            return (
+                <h2
+                    style={{
+                        color: `var(--${color})`,
+                        fontSize: FontSize[type]
+                    }}
+                    className={styles['text__h2']}
+                >
+                    {children}
+                </h2>
+            )
+        case 'h3':
+            return (
+                <h3
+                    style={{
+                        color: `var(--${color})`,
+                        fontSize: FontSize[type]
+                    }}
+                    className={styles['text__h3']}
+                >
+                    {children}
+                </h3>
+            )
+        default:
+            return (
+                <p
+                    style={{
+                        color: `var(--${color})`,
+                        fontSize: FontSize[type]
+                    }}
+                    className={styles['text__body']}
+                >
+                    {children}
+                </p>
+            )
+    }
 }
