0b2ee92a642098630884ed7b97825974ead12279
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-03-23 12:05:31 +0100

    refactor(button): refactored button component
    Added explicit props for width and height so to have better control over
    the component dimensions.

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,18 +3,23 @@ import { MouseEventHandler, ReactNode } from 'react'
 import styles from './button.module.scss'
 
 interface ButtonProps {
+    width?: string | number
+    height?: string | number
     background?: 'light' | 'dark'
     onClick?: MouseEventHandler<HTMLButtonElement>
     children: ReactNode
 }
 
 export default function Button({
+    width = 152,
+    height = 56,
     background = 'light',
     onClick,
     children
 }: ButtonProps) {
     return (
         <button
+            style={{ width, height }}
             className={clsx(styles['button'], {
                 [styles['button--light']]: background === 'light',
                 [styles['button--dark']]: background === 'dark'
